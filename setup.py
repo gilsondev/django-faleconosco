89d203829ec887fd0f77f94492a432b12793e0bf
Gilson Filho <contatogilsonsbf@gmail.com>
2011-11-03 15:11:03 -0200

    Setup alterado, baseado no projeto django-bootstrap

--- setup.py
+++ setup.py
@@ -1,43 +1,46 @@
 # -*- coding: utf8 -*-
 
-from setuptools import setup
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
 import os
- 
- 
-README_FILE = open('README.md')
-try:
-    LONG_DESCRIPTION = README_FILE.read()
-finally:
-    README_FILE.close()
- 
-ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-DATA_DIR = os.path.join(ROOT_DIR, 'contato', 'templates')
-TEMPLATE_DATA = []
-for path, dirs, filenames in os.walk(DATA_DIR):
-    # Ignore directories that start with '.'
-    for i, dir in enumerate(dirs):
-        if dir.startswith('.'):
-            del dirs[i]
-    path = path[len(DATA_DIR) + 1:]
-    TEMPLATE_DATA.append(os.path.join('templates', path, '*.*'))
-    # Get files starting with '.' too (they are excluded from the *.* glob).
-    TEMPLATE_DATA.append(os.path.join('templates', path, '.*'))
- 
- 
-setup(name='django-faleconosco',
-      version='1.0a',
-      author='Gilson Filho',
-      author_email='contato@gilsondev.com',
-      description=('Aplicação responsável pela seção Fale Conosco de sites, portais, blogs, etc.'),
-      long_description=LONG_DESCRIPTION,
-      packages=['contato'],
-      package_data={'contato': TEMPLATE_DATA},
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Web Environment',
-          'Framework :: Django',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules'])
+
+root = os.path.dirname(os.path.abspath(__file__))
+os.chdir(root)
+
+VERSION = '1.1a'
+
+# Make data go to the right place.
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+def gen_data_files(*dirs):
+    results = []
+    for src_dir in dirs:
+        for root,dirs,files in os.walk(src_dir):
+            results.append((root, map(lambda f:root + "/" + f, files)))
+    return results
+
+setup(
+    name='django-faleconosco',
+    version=VERSION,
+    description="Aplicacao responsavel pela secao Fale Conosco de sites, portais, blogs, etc.",
+    long_description="Essa aplicacao foi feita para evitar o retrabalho no desenvolvimento da pagina de contato do seu site, blog ou portal..",
+    author="Gilson Filho",
+    author_email="contato@gilsondev.com",
+    url="http://github.com/gilsondev/django-faleconosco",
+    license="BSD License",
+    platforms=["any"],
+    packages=['contato'],
+    data_files=gen_data_files(os.path.join('contato', 'templates')),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    include_package_data=True,
+)
